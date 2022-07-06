37f08257f67f7db85a9562f38532f42e72af7059
Bob Pulaski Home <mynameisempty@yandedex.ru>
2022-07-06 18:08:10 +0300

    #1 Создание таблиц

---
+++ database/migrations/2022_07_06_112759_create_rules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rules', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('header_id')->constrained ()->onDelete ('cascade');
+            $table->string('rule_left');
+            $table->string('rule_right');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rules');
+    }
+};
