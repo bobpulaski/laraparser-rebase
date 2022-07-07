ebedd739eb9de4f576b546d859d87252378b465f
bobpulaski <82018695+bobpulaski@users.noreply.github.com>
2022-07-07 15:17:07 +0300

    #2 Установка Admin LTE

--- resources/views/__board.blade.php
+++ resources/views/__board.blade.php
@@ -1,6 +1,11 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight bg-indigo-50">
+            {{ __('Dashboard') }}
+            <h1>Hello!</h1>
 
-@section('content')
+        </h2>
+    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -11,6 +16,4 @@
             </div>
         </div>
     </div>
-
-@endsection
-
+</x-app-layout>
